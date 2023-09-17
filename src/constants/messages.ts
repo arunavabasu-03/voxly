d8821f2ca8a20da661a6548237fef7ea28e36640
Arunava Basu <arunavabasudev@gmail.com>
2023-09-17 20:41:48 +0530

    feat: adding voice feature

--- src/constants/messages.ts
+++ src/constants/messages.ts
@@ -1,4 +1,4 @@
-export const apiKey = "place your openAI api key here";
+export const apiKey = "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";
 // in some cases your api key maybe already expired
 // try to use a new account to create an api key
 
